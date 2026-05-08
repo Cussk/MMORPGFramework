962bc75bc47807484c01f01c82259a5d70bbafdf
Kyle Cuss <cussprogramming@gmail.com>
2026-05-08 08:54:13 -0230

    Added core asset registry subsystem

---
+++ Plugins/MDF/Source/MDFFrameworkCore/Private/Subsystems/MDFDefinitionRegistrySubsystem.cpp
@@ -0,0 +1,72 @@
+﻿// Kyle Cuss and Cuss Programming 2026
+
+#include "Subsystems/MDFDefinitionRegistrySubsystem.h"
+
+#include "Data/MDFDefinitionAsset.h"
+
+bool UMDFDefinitionRegistrySubsystem::RegisterDefinition(
+	const FGameplayTag DefinitionTag,
+	UMDFDefinitionAsset* Definition)
+{
+	if (!DefinitionTag.IsValid() || !Definition)
+	{
+		return false;
+	}
+
+	if (TObjectPtr<UMDFDefinitionAsset>* ExistingDefinition = RegisteredDefinitions.Find(DefinitionTag))
+	{
+		if (ExistingDefinition->Get() == Definition)
+		{
+			return true;
+		}
+
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("MDFDefinitionRegistrySubsystem: Replacing definition registered for tag [%s]. Old=[%s], New=[%s]"),
+			*DefinitionTag.ToString(),
+			*GetNameSafe(ExistingDefinition->Get()),
+			*GetNameSafe(Definition));
+	}
+
+	RegisteredDefinitions.Add(DefinitionTag, Definition);
+	return true;
+}
+
+int32 UMDFDefinitionRegistrySubsystem::RegisterDefinitions(
+	const TMap<FGameplayTag, UMDFDefinitionAsset*>& Definitions)
+{
+	int32 RegisteredCount = 0;
+
+	for (const TPair<FGameplayTag, UMDFDefinitionAsset*>& DefinitionPair : Definitions)
+	{
+		if (RegisterDefinition(DefinitionPair.Key, DefinitionPair.Value))
+		{
+			++RegisteredCount;
+		}
+	}
+
+	return RegisteredCount;
+}
+
+void UMDFDefinitionRegistrySubsystem::ClearRegisteredDefinitions()
+{
+	RegisteredDefinitions.Reset();
+}
+
+bool UMDFDefinitionRegistrySubsystem::HasDefinition(const FGameplayTag DefinitionTag) const
+{
+	return DefinitionTag.IsValid() && RegisteredDefinitions.Contains(DefinitionTag);
+}
+
+const UMDFDefinitionAsset* UMDFDefinitionRegistrySubsystem::ResolveDefinition(
+	const FGameplayTag DefinitionTag) const
+{
+	if (!DefinitionTag.IsValid())
+	{
+		return nullptr;
+	}
+
+	const TObjectPtr<UMDFDefinitionAsset>* Definition = RegisteredDefinitions.Find(DefinitionTag);
+	return Definition ? Definition->Get() : nullptr;
+}
---
+++ Plugins/MDF/Source/MDFFrameworkCore/Public/Subsystems/MDFDefinitionRegistrySubsystem.h
@@ -0,0 +1,55 @@
+﻿// Kyle Cuss and Cuss Programming 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MDFDefinitionRegistrySubsystem.generated.h"
+
+class UMDFDefinitionAsset;
+
+/**
+ * Lightweight runtime registry for authored MDF definitions.
+ *
+ * This subsystem does not scan content or own gameplay rules. Feature modules
+ * explicitly register definitions using their domain tags, such as SkillTag,
+ * DisciplineTag, ItemTag, RecipeTag, or WorldTag.
+ */
+UCLASS()
+class MDFFRAMEWORKCORE_API UMDFDefinitionRegistrySubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="MDF|Definitions")
+	bool RegisterDefinition(FGameplayTag DefinitionTag, UMDFDefinitionAsset* Definition);
+
+	UFUNCTION(BlueprintCallable, Category="MDF|Definitions")
+	int32 RegisterDefinitions(const TMap<FGameplayTag, UMDFDefinitionAsset*>& Definitions);
+
+	UFUNCTION(BlueprintCallable, Category="MDF|Definitions")
+	void ClearRegisteredDefinitions();
+
+	UFUNCTION(BlueprintPure, Category="MDF|Definitions")
+	bool HasDefinition(FGameplayTag DefinitionTag) const;
+
+	UFUNCTION(BlueprintPure, Category="MDF|Definitions")
+	const UMDFDefinitionAsset* ResolveDefinition(FGameplayTag DefinitionTag) const;
+
+	template<typename DefinitionType>
+	const DefinitionType* ResolveDefinition(FGameplayTag DefinitionTag) const
+	{
+		return Cast<DefinitionType>(ResolveDefinition(DefinitionTag));
+	}
+
+	UFUNCTION(BlueprintPure, Category="MDF|Definitions")
+	int32 GetRegisteredDefinitionCount() const
+	{
+		return RegisteredDefinitions.Num();
+	}
+
+protected:
+	UPROPERTY(Transient)
+	TMap<FGameplayTag, TObjectPtr<UMDFDefinitionAsset>> RegisteredDefinitions;
+};
